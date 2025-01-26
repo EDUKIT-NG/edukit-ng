94d28960869595ed2444508d641eee1158ca15a4
feco-dev365 <donfecoonline@gmail.com>
2025-01-26 23:41:43 +0100

    removed our partners section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,6 @@ import KeyFeatures from "../components/KeyFeatures/keyfeatures";
 import ImpactSection from "../components/ImpactSection/ImpactSection";
 import TestimonialSection from "../components/TestimonialSection/TestimonialSection";
 import SuccessStories from "../components/SuccessStories/SuccessStories";
-import PartnersSection from "../components/PartnersSection/PartnersSection";
 import MissionSupport from "../components/MissionSupport/MissionSupport";
 import NewsletterFooter from "../components/NewsletterFooter/NewsletterFooter";
 
@@ -18,7 +17,6 @@ const Home = () => {
       <ImpactSection />
       <TestimonialSection />
       <SuccessStories />
-      <PartnersSection />
       <MissionSupport />
       <NewsletterFooter />
     </div>
