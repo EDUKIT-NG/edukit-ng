46b1120620d3601bfdc32d3b01538a65e787d80d
winifred29 <winifreddd29@gmail.com>
2025-01-31 21:18:49 +0100

    fixes:reorganise folders.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import HeroSection from "../components/Hero/HeroSection";
 import KeyFeatures from "../components/KeyFeatures/keyfeatures";
-import ImpactSection from "../components/ImpactSection/ImpactSection";
-import TestimonialSection from "../components/TestimonialSection/TestimonialSection";
-import ProjectsSection from "../components/ProjectsSection/ProjectsSection";
-import MissionSupport from "../components/MissionSupport/MissionSupport";
-import NewsletterFooter from "../components/NewsletterFooter/NewsletterFooter";
-
+import ImpactSection from "../components/Home/ImpactSection/ImpactSection";
+import TestimonialSection from "../components/Home/TestimonialSection/TestimonialSection";
+import ProjectsSection from "../components/Home/ProjectsSection/ProjectsSection";
+import MissionSupport from "../components/Home/MissionSupport/MissionSupport";
+import NewsletterFooter from "../components/Home/NewsletterFooter/NewsletterFooter";
 
 const Home = () => {
   return (
@@ -16,7 +15,7 @@ const Home = () => {
       <KeyFeatures />
       <ImpactSection />
       <TestimonialSection />
-      <ProjectsSection/>
+      <ProjectsSection />
       <MissionSupport />
       <NewsletterFooter />
     </div>
