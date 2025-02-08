4e0f84f101e6c88783e53319e6960a2a3dbba687
Shedrack Okunda <okundashedrack@gmail.com>
2025-02-08 10:01:14 +0300

    Modified the code by replacing the require method with import due to ES Module starndards and due to vite installation and debugged the error on the console which was Uncaught ReferrenceError: require is not defined

--- src/components/Home/ProjectsSection/ProjectsSection.jsx
+++ src/components/Home/ProjectsSection/ProjectsSection.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./ProjectsSection.css";
+import project1 from "./project.1.png";
+import project2 from "./project.2.png";
+import project3 from "./project.3.png";
 
 const ProjectsSection = () => {
   const [projects] = useState([
@@ -8,63 +11,63 @@ const ProjectsSection = () => {
       title: "Uniform Distribution and School Supplies Initiative",
       description:
         "Edukit Nigeria provided school uniforms, bags, and stationery to students at Hope Academy, Oyo State.",
-      img: require("./project.1.png"),
+      img: project1,
     },
     {
       id: 2,
       title: "Community Fundraising Drive",
       description:
         "Edukit Nigeria hosted an engaging fundraising drive to generate support for it’s mission.",
-      img: require("./project.2.png"),
+      img: project2,
     },
     {
       id: 3,
       title: "Community Library Setup",
       description:
         "Edukit Nigeria established a community library at Unity Secondary School in Sokoto State, Nigeria.",
-      img: require("./project.3.png"),
+      img: project3,
     },
     {
       id: 4,
       title: "Project 4",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in dui ultrices, nec ultricies nunc varius.",
-      img: require("./project.3.png"),
+      img: project3,
     },
     {
       id: 5,
       title: "Project 5",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in dui ultrices, nec ultricies nunc varius.",
-      img: require("./project.3.png"),
+      img: project3,
     },
     {
       id: 6,
       title: "Project 6",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in dui ultrices, nec ultricies nunc varius.",
-      img: require("./project.3.png"),
+      img: project3,
     },
     {
       id: 7,
       title: "Project 7",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in dui ultrices, nec ultricies nunc varius.",
-      img: require("./project.3.png"),
+      img: project3,
     },
     {
       id: 8,
       title: "Project 8",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in dui ultrices, nec ultricies nunc varius.",
-      img: require("./project.3.png"),
+      img: project3,
     },
     {
       id: 9,
       title: "Project 9",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in dui ultrices, nec ultricies nunc varius.",
-      img: require("./project.3.png"),
+      img: project3,
     },
   ]);
 
