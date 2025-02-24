cfdfa6e8840cc90861c0cea6ad1a0bee18970141
Ayomide <midedev001@gmail.com>
2025-02-24 19:22:52 +0100

    about page

--- src/Pages/OurProfile.jsx
+++ src/Pages/OurProfile.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import AboutUs from "../components/AboutUs/Aboutus";
 const OurProfile = () => {
     return ( 
         <div>
-            
+            <AboutUs/>
         </div>
     );
 };
