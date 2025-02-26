dc8b42027caf7a44e7300c0f0a6a24752d67339f
winifred29 <winifreddd29@gmail.com>
2025-02-26 22:52:47 +0100

    fixes

--- src/Pages/OurProfile.jsx
+++ src/Pages/OurProfile.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutUs from "../components/AboutUs/Aboutus";
+import SDGSection from "../components/OurProfile/SDGSection/SDG";
 const OurProfile = () => {
     return ( 
         <div>
             <AboutUs/>
+            <SDGSection/>
         </div>
     );
 };
