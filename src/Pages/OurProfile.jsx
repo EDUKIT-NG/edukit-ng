1a993c89a81b1656faed9a355b4a47efbb14ba3e
Agu Gideon <agugideon409@gmail.com>
2025-02-28 13:16:29 +0100

    fixed the issue on the sDG section

--- src/Pages/OurProfile.jsx
+++ src/Pages/OurProfile.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AboutUs from "../components/AboutUs/Aboutus";
-import SDGsection from "../components/OurProfile/SDGsection/SDG";
+import SDG from "../components/OurProfile/SDGSection/SDG";
 const OurProfile = () => {
-    return ( 
-        <div>
-            <AboutUs/>
-            <SDGsection/>
-        </div>
-    );
+  return (
+    <div>
+      <AboutUs />
+      <SDG />
+    </div>
+  );
 };
 
 export default OurProfile;
