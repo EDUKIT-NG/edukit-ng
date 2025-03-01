c12f07cb4e9f4e43c3eaf99c5173903dc811ba4a
Agu Gideon <agugideon409@gmail.com>
2025-03-01 14:55:30 +0100

    corrections to the home page

--- src/Pages/OurProfile.jsx
+++ src/Pages/OurProfile.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AboutUs from "../components/AboutUs/Aboutus";
-import SDGsection from "../components/OurProfile/SDGsection/SDG";
+import SDGsection from "../components/OurProfile/SDGSection/SDG";
 const OurProfile = () => {
-    return ( 
-        <div>
-            <AboutUs/>
-            <SDGsection/>
-        </div>
-    );
+  return (
+    <div>
+      <AboutUs />
+      <SDGsection />
+    </div>
+  );
 };
 
 export default OurProfile;
