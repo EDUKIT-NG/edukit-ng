cf0b8bef55a080c1784ad2303b1f67ce79a78140
winifred29 <winifreddd29@gmail.com>
2025-03-01 19:53:29 +0100

    worked on sdg section

--- src/Pages/OurProfile.jsx
+++ src/Pages/OurProfile.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import AboutUs from "../components/AboutUs/Aboutus";
 import SDGsection from "../components/OurProfile/SDGsection/SDG";
+import ImpactSection from "../components/Home/ImpactSection/ImpactSection";
+import NewsletterFooter from "../components/Home/NewsletterFooter/NewsletterFooter";
 const OurProfile = () => {
     return ( 
         <div>
             <AboutUs/>
             <SDGsection/>
+            <ImpactSection/>
+            <NewsletterFooter/>
         </div>
     );
 };
