4e678163e46e62f0c01382b528ab8ac031e22b72
feco-dev365 <donfecoonline@gmail.com>
2025-03-12 18:54:41 +0100

    add OurHolisticApproach

--- src/Pages/OurProfile.jsx
+++ src/Pages/OurProfile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutUs from "../components/AboutUs/Aboutus";
+import OurHolisticApproach from "../components/OurProfile/OurHolisticApproach";
 import SDG from "../components/OurProfile/SDGSection/SDG";
 import ImpactSection from "../components/Home/ImpactSection/ImpactSection";
 import NewsletterFooter from "../components/Home/NewsletterFooter/NewsletterFooter";
@@ -7,6 +8,7 @@ const OurProfile = () => {
   return (
     <div>
       <AboutUs />
+      <OurHolisticApproach />
       <SDG />
       <ImpactSection />
       <NewsletterFooter />
