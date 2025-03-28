38ca731d01b9b355b0f29884ba7b0d9e570ab93e
Ayomide <midedev001@gmail.com>
2025-03-28 04:18:48 +0100

    E-learning sec

--- src/Pages/OurStory.jsx
+++ src/Pages/OurStory.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Learning from "../components/E-learning/Learning";
 
 const OurStory = () => {
   return (
     <div>
-        
+        <Learning/>
     </div>
   );
 };
